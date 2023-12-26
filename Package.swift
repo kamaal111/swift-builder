30e30b050f6e88dfe4137162bb20028b2ec0348f
Kamaal Farah <kamaal.f1@gmail.com>
2023-12-26 21:58:23 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import CompilerPluginSupport
+
+let package = Package(
+    name: "swift-builder",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "swift-builder",
+            targets: ["swift-builder"]
+        ),
+        .executable(
+            name: "swift-builderClient",
+            targets: ["swift-builderClient"]
+        ),
+    ],
+    dependencies: [
+        // Depend on the Swift 5.9 release of SwiftSyntax
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        // Macro implementation that performs the source transformation of a macro.
+        .macro(
+            name: "swift-builderMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+
+        // Library that exposes a macro as part of its API, which is used in client programs.
+        .target(name: "swift-builder", dependencies: ["swift-builderMacros"]),
+
+        // A client of the library, which is able to use the macro in its own code.
+        .executableTarget(name: "swift-builderClient", dependencies: ["swift-builder"]),
+
+        // A test target used to develop the macro implementation.
+        .testTarget(
+            name: "swift-builderTests",
+            dependencies: [
+                "swift-builderMacros",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ]
+        ),
+    ]
+)
