52c82bf4a658d18425474a279a7eca0bca100da9
Kamaal Farah <kamaal.f1@gmail.com>
2023-12-31 21:49:03 +0100

    Testing error cases

--- Tests/SwiftBuilderTests/BuilderSnapshotTests.swift
+++ Tests/SwiftBuilderTests/BuilderSnapshotTests.swift
@@ -35,6 +35,28 @@ final class BuilderSnapshotTests: XCTestCase {
         )
     }
 
+    func testBuilderMacroUnsupportedType() throws {
+        let snapshotName = try XCTUnwrap(#function.split(separator: "()").first)
+        let snapshotNameString = String(snapshotName)
+        assertMacroExpansion(
+            try getSnapshot(named: snapshotNameString, ofType: .source),
+            expandedSource: try getSnapshot(named: snapshotNameString, ofType: .expanded),
+            diagnostics: [.init(message: "@Builder only supports classes", line: 1, column: 1)],
+            macros: testMacros
+        )
+    }
+
+    func testBuilderMacroInsufficientProperties() throws {
+        let snapshotName = try XCTUnwrap(#function.split(separator: "()").first)
+        let snapshotNameString = String(snapshotName)
+        assertMacroExpansion(
+            try getSnapshot(named: snapshotNameString, ofType: .source),
+            expandedSource: try getSnapshot(named: snapshotNameString, ofType: .expanded),
+            diagnostics: [.init(message: "Must have atleast mutable 1 property", line: 1, column: 1)],
+            macros: testMacros
+        )
+    }
+
     private func getSnapshot(named name: String, ofType type: SnapshotTypes) throws -> String {
         let path = try XCTUnwrap(Bundle.module.path(forResource: name, ofType: type.rawValue))
         let url = URL(fileURLWithPath: path)
---
+++ Tests/SwiftBuilderTests/BuilderTests.swift
@@ -0,0 +1,11 @@
+//
+//  BuilderTests.swift
+//
+//
+//  Created by Kamaal M Farah on 31/12/2023.
+//
+
+import XCTest
+
+final class BuilderTests: XCTestCase {
+}
